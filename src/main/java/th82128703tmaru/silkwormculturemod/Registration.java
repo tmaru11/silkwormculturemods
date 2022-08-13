94903af9c0622fd5215442d0964430305eaf984c
tmaru11 <takehonomaru@gmail.com>
2022-08-13 13:52:19 +0900

    aa

--- src/main/java/th82128703tmaru/silkwormculturemod/Registration.java
+++ src/main/java/th82128703tmaru/silkwormculturemod/Registration.java
@@ -22,9 +22,9 @@ public class Registration {private static final DeferredRegister<Block> BLOCKS =
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         BLOCKS.register(bus);
         ITEMS.register(bus);}public static final BlockBehaviour.Properties BLOCK_PROPERTIES = BlockBehaviour.Properties.of(Material.GRASS).strength(1f).sound(SoundType.GRASS).requiresCorrectToolForDrops().destroyTime(0f).noCollission().noOcclusion();
-    public static final Item.Properties ITEM_PROPERTIES = new Item.Properties().tab(ModSetup.TAB);public static final RegistryObject<Block>
+    public static final Item.Properties FOOD_PROPERTIES = new Item.Properties().tab(ModSetup.TAB);public static final RegistryObject<Block>
             TESTBLOCK = BLOCKS.register("testblock", () -> new CustomPlantBlock(BLOCK_PROPERTIES));
-    public static final RegistryObject<Item> TESTBLOCK_ITEM = fromBlock(TESTBLOCK, ITEM_PROPERTIES.food(new FoodProperties.Builder().nutrition(1).saturationMod(0.2F).build()));
+    public static final RegistryObject<Item> TESTBLOCK_ITEM = fromBlock(TESTBLOCK,FOOD_PROPERTIES=new Item.Properties().tab(ModSetup.TAB).food(new FoodProperties.Builder().nutrition(1).saturationMod(0.2F).build()));
 
     public static final BlockBehaviour.Properties BLOCK_PROPERTIES2 = BlockBehaviour.Properties.of(Material.GRASS).strength(2f).sound(SoundType.GRASS).destroyTime(1f).noOcclusion().requiresCorrectToolForDrops();
     public static final RegistryObject<Block> TESTBLOCK2 = BLOCKS.register("testblock2_id", () -> new Block(BLOCK_PROPERTIES2));
@@ -34,6 +34,7 @@ public class Registration {private static final DeferredRegister<Block> BLOCKS =
     public static final RegistryObject<Block> TESTBLOCK3 = BLOCKS.register("testblock3", () -> new Block(BLOCK_PROPERTIES3));
 
     public static final RegistryObject<Item> TESTBLOCK3_ITEM = fromBlock(TESTBLOCK3,ITEM_PROPERTIES);
+
 
     public static final RegistryObject<Item> MULBERRYKNIFE = ITEMS.register("mulberryknife", () -> new Item(ITEM_PROPERTIES));
 
